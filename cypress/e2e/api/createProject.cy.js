c0f58f3dd41e992a8e48ff25d22ff882eda9407f
jonatas-campista <jonatasc5622@gmail.com>
2024-10-21 19:01:16 -0300

    api getAllProjects and deleteProjects

--- cypress/e2e/api/createProject.cy.js
+++ cypress/e2e/api/createProject.cy.js
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker'
 
-describe('Create Project', () => {
+describe('Create project', () => {
+  beforeEach(() => cy.api_deleteProjects())
+
   it('successfully', () => {
     const project = {
       name: `project-${faker.datatype.uuid()}`,
@@ -13,23 +15,5 @@ describe('Create Project', () => {
         expect(response.body.name).to.equal(project.name)
         expect(response.body.description).to.equal(project.description)
       })
-  })
-
-  Cypress.Commands.add('api_getAllProjects', () => {
-    cy.request({
-      method: 'GET',
-      url: '/api/v4/projects/',
-      headers: { Authorization: accessToken },
-    })
-  })
-  
-  Cypress.Commands.add('api_deleteProjects', () => {
-    cy.api_getAllProjects().then(res =>
-      res.body.forEach(project => cy.request({
-        method: 'DELETE',
-        url: `/api/v4/projects/${project.id}`,
-        headers: { Authorization: accessToken },
-      }))
-    )
   })
 })
